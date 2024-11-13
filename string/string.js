6609148863d2b3f124bfa7ab8314ceb5adf98b8d
Syedsamid <samidsyed1720@gmail.com>
2024-11-12 20:05:12 -1200

    string methods example

---
+++ string/string.js
@@ -0,0 +1,19 @@
+// let str  = "5";
+
+// let check =  str.padStart(10, "8");
+// console.log(check);
+
+// let str = "hello world samid syed heyy";
+
+// let check = str.split(' ');
+// console.log(check);
+
+// let str1 = "Hello World";
+// let check1 = str1.startsWith("hello");
+// console.log(check1);
+
+
+let str3 = "Hello ";
+let check2 = str3.repeat(100);
+console.log(check2);
+
