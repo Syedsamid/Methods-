1c32cbdba26d52e6c8dd898b11c7ed19cdcc1d0d
Syedsamid <samidsyed1720@gmail.com>
2024-11-12 14:36:55 -1200

    isArray

--- array/13.isArray().js
+++ array/13.isArray().js
@@ -1,7 +1,7 @@
 // isArray
 
-//The Array.isArray() methods checks if a given value is an array, returning 
-// true if it is and false otherwise.
+//* The Array.isArray() methods checks if a given value is an array, returning 
+//* true if it is and false otherwise.
 
 let a = "Hello";
 let b = 10;
@@ -13,31 +13,4 @@ let e = [10, "Hello", false, true, 34, "Bye"];
 
 console.log(Array.isArray(e));
 
-
-let arr2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-console.log(arr2);
-
-console.log(Array.isArray(arr2));
-
-console.log(typeof arr2); //? Its an Object but shows as Array
-
-
-// passing an empty array []
-console.log(Array.isArray([])); // true
-
-
-// we have created an array with element 7 and
-// passed that value to isArray()
-console.log(Array.isArray(new Array(7))); // true
-
-// passing a boolean value
-console.log(Array.isArray(true)); // false
-
-
-// passing undefined
-console.log(Array.isArray(undefined)); // false
-
-
-// not passing any argument in isArray()
-console.log(Array.isArray()); // false
 
